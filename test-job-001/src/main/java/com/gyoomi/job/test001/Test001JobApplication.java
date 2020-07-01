b83a4dba473e98d42624437f15ac632fb394a64a
leon <gyoomi0709@foxmail.com>
2020-07-01 22:29:02 +0800

    Initial commit

---
+++ test-job-001/src/main/java/com/gyoomi/job/test001/Test001JobApplication.java
@@ -0,0 +1,21 @@
+package com.gyoomi.job.test001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author Leon
+ * @version 2020/7/1 22:06
+ */
+@SpringBootApplication
+public class Test001JobApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Test001JobApplication.class, args);
+	}
+
+}
---
+++ test-job-002/src/main/java/com/gyoomi/job/test002/Test002JobApplication.java
@@ -0,0 +1,21 @@
+package com.gyoomi.job.test002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author Leon
+ * @version 2020/7/1 22:15
+ */
+@SpringBootApplication
+public class Test002JobApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Test002JobApplication.class, args);
+	}
+
+}
